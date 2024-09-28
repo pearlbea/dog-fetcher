8e3ab0041721b3a2741d300e2c5bdddbe22ea022
Pearl Latteier <pearlbea@gmail.com>
2024-09-28 12:56:32 -0500

    Update breed selection

--- app/dashboard/breed-list.tsx
+++ app/dashboard/breed-list.tsx
@@ -1,26 +1,16 @@
 "use client";
 
+import { ChangeEvent, ChangeEventHandler } from "react";
 import useSWR from "swr";
 import { breeds } from "../requests/breeds";
-import {
-  Button,
-  Checkbox,
-  CheckboxGroup,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@chakra-ui/react";
+import { Select, Spinner } from "@chakra-ui/react";
 
 export function BreedList({
-  onChange,
+  onChangeHandler,
 }: {
-  onChange: (value: string[]) => void;
+  onChangeHandler: (event: ChangeEvent<HTMLSelectElement>) => void;
 }) {
   const { data, error, isLoading } = useSWR("BREEDS", breeds);
-
-  function handleChange(value: string[]) {
-    onChange(value);
-  }
 
   if (error) {
     return <div>Error</div>;
@@ -31,29 +21,12 @@ export function BreedList({
   }
 
   return (
-    <Popover placement="bottom-start">
-      <PopoverTrigger>
-        <Button>Filter by breed</Button>
-      </PopoverTrigger>
-      <PopoverContent maxH="64" overflow="scroll">
-        <CheckboxGroup colorScheme="pink" onChange={handleChange}>
-          {data.map((breed: string) => (
-            <Checkbox
-              _active={{ backgroundColor: "pink.100", color: "gray.900" }}
-              _focus={{ backgroundColor: "pink.100", color: "gray.900" }}
-              _hover={{ backgroundColor: "pink.100", color: "gray.900" }}
-              key={breed}
-              name={breed}
-              px="4"
-              py="2"
-              role="option"
-              value={breed}
-            >
-              {breed}
-            </Checkbox>
-          ))}
-        </CheckboxGroup>
-      </PopoverContent>
-    </Popover>
+    <Select placeholder="Filter by breed" w="300px" onChange={onChangeHandler}>
+      {data.map((breed: string) => (
+        <option key={breed} value={breed}>
+          {breed}
+        </option>
+      ))}
+    </Select>
   );
 }
